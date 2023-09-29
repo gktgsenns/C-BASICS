417144cb35ff2d51037311a42bdd22dbafb4f88d
gktgsenns <139327946+gktgsenns@users.noreply.github.com>
2023-09-29 23:37:53 +0300

    Add files via upload

---
+++ WriteINTValue.C
@@ -0,0 +1,21 @@
+/*
+A SIMPLE C CODE.
+THIS CODE PRINTS THE NUMBER A AND B UNDER THE BOTTOM.
+*/
+#include<stdio.h>
+
+int main(){
+   int a = 5;
+int b = 3;
+printf("%d",a
+);   printf("\n%d",b);
+
+
+//IF YOU WANT EXIT ON ANY KEY PRESS YOU CAN ADD 
+
+//THIS CODE HERE...
+
+
+//EXIT
+return 0;
+}
