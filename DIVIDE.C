7e6c42719e7b7b91b144677b49e14743f0b4ff59
gktgsenns <139327946+gktgsenns@users.noreply.github.com>
2023-09-29 23:58:41 +0300

    Add files via upload

---
+++ DIVIDE.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int dividend = 10;
+    int divisor = 5;
+    int result;
+
+
+// THIS CODE DIVIDES THE NUMBER 10 BY 5.
+    result = dividend / divisor;
+
+    printf("%d / %d = %d\n", dividend, divisor, result);
+
+    return 0;
+}
