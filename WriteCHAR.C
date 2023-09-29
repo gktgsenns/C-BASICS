0df138fd379e53b15b0432e9258a59f4d9666e15
gktgsenns <139327946+gktgsenns@users.noreply.github.com>
2023-09-29 23:50:46 +0300

    Add files via upload

---
+++ WriteCHAR.C
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main(){
+    char char1  = 'c';
+    printf("%c",char1
+    );
+
+
+
+
+/*
+YOU WILL NEED TO HAVE DIFFERENT CHAR VARIABLES TO PRINT THE SENTENCES UNDER THE BOTTOM.
+
+EXAMPLE USAGE :
+
+
+CHAR CHARH = 'H';
+
+CHAR CHARE = 'E';
+
+CHAR CHARL = 'L';
+
+CHAR CHARL = 'L';
+
+CHAR CHARO = 'O';
+
+
+
+
+//---------------
+
+ printf("\n%c",charH
+  printf("\n%c",charE
+   printf("\n%c",charL
+    printf("\n%c",charL
+     printf("\n%c",charO
+*/
+
+
+
+
+
+
+
+
+return 0;
+
+}
